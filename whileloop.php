4ff3ab47ff5cab27056056e4c115775a02b8a6ba
Emzelle <145777871+Emzelle@users.noreply.github.com>
2024-10-14 17:35:36 +0800

    first commit

---
+++ whileloop.php
@@ -0,0 +1,14 @@
+<?php
+$count = 1;
+while ($count <= 10) {
+    echo $count ." ";
+    $count++;
+}
+echo"\n";
+$count = 2;
+while ($count <= 20) {
+    echo $count ." ";
+    $count += 2;
+}
+
+?>
