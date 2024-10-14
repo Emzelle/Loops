10acd56ca5d3256b89980df9e7c72f09af0fa614
Emzelle <145777871+Emzelle@users.noreply.github.com>
2024-10-14 19:14:20 +0800

    Act6 done

---
+++ Act6.php
@@ -0,0 +1,7 @@
+<?php
+$movies = array("1. Spider-Man: Into the Spider-Verse", "2. Inception", "3. Forest Gump", "4. Avengers", "5. Shin Kamen Rider");
+
+foreach ($movies as $x) {
+  echo "$x <br>";
+}
+?>
